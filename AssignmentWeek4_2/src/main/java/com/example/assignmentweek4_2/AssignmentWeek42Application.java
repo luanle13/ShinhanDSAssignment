264fda2c648b32ebeebf75ac9b544e333675defb
Luan Le <luanle13>
2023-01-04 23:22:16 +0700

    add assignment week 4-2

---
+++ AssignmentWeek4_2/src/main/java/com/example/assignmentweek4_2/AssignmentWeek42Application.java
@@ -0,0 +1,13 @@
+package com.example.assignmentweek4_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWeek42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentWeek42Application.class, args);
+    }
+
+}
---
+++ AssignmentWeek4_2/src/test/java/com/example/assignmentweek4_2/AssignmentWeek42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentweek4_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWeek42ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
