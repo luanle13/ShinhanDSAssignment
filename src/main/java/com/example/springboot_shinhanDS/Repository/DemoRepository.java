06eab3880b0e175d52b3dfc8ebd9fc743e151cfd
Luan Le <luanle13>
2023-01-11 10:58:23 +0700

    add assignment week 5-1

---
+++ src/main/java/com/example/springboot_shinhanDS/Repository/DemoRepository.java
@@ -0,0 +1,8 @@
+package com.example.springboot_shinhanDS.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DemoRepository {
+    public String printScreenRepo = "Hello world, Xuan An";
+}
